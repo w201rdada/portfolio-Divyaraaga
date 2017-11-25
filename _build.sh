3580f2b7bff1fd06b3f811676ae85e2bb5737ea4
Divyaraaga <divya.raaga@gmail.com>
2017-11-25 22:39:24 +0000

    deploy

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 pkill caddy
 cd /home/oski/ && rm -rf _book/ _main*md \
-&& Rscript -e 'bookdown::render_book("01.Rmd", "bookdown::gitbook")' \
+&& Rscript -e 'bookdown::render_book("index.Rmd", "bookdown::gitbook")' \
 && cd /home/oski/_book && caddy
